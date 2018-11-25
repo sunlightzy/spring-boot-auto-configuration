91506c8d44724ef184d7437cbb902aee610aeae7
sunzhenyu <sunzhenyu@terminus.io>
2018-11-25 16:28:21 +0800

    init

---
+++ src/main/java/com/glmapper/spring/demo/SpringBootAutoConfigurationApplication.java
@@ -0,0 +1,18 @@
+package com.glmapper.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBootAutoConfigurationApplication
+ *
+ * @author zhenyu.szy
+ * @date 2018-11-25
+ */
+@SpringBootApplication
+public class SpringBootAutoConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoConfigurationApplication.class, args);
+    }
+}
